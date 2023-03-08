bc578b7d71092bdffd3a9b49db0dedc86435dea7
Thomas Bartlett <116368168+thom-bartlett@users.noreply.github.com>
2023-03-08 14:51:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 from flask import Flask
-from flask import render_template
+import logging
+from slack_bolt import App
+from slack_bolt.adapter.flask import SlackRequestHandler
 
-app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG)
+app = App()
 
-slack_app = App(process_before_response=True)
+@app.middleware  # or app.use(log_request)
+def log_request(logger, body, next):
+    logger.debug(body)
+    return next()
 
+@app.event("app_mention")
+def event_test(body, say, logger):
+    logger.info(body)
+    say("What's up?")
+
+@app.event("message")
+def handle_message():
+    pass
+
+from flask import Flask, request
+
+flask_app = Flask(__name__)
 handler = SlackRequestHandler(app)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+@flask_app.route("/slack/events", methods=["POST"])
+def slack_events():
+    return handler.handle(request)
+
